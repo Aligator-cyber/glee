43df3e5ec9794167e0511ada07aa37b6bf1df39b
Aligator-cyber <o.tim.2014@mail.ru>
2021-05-04 15:48:25 +0300

    finished this page

--- app/js/main.js
+++ app/js/main.js
@@ -28,6 +28,31 @@ $(function() {
         arrows: false,
         slidesToShow: 5,
         slidesToScroll: 1,
+        responsive: [{
+                breakpoint: 1199,
+                settings: {
+                    slidesToShow: 4,
+                }
+            },
+            {
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                }
+            }, {
+                breakpoint: 440,
+                settings: {
+                    slidesToShow: 1,
+                }
+            },
+        ]
+
 
     })
 })
